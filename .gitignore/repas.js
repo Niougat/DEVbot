32ff44ce41a59f6887895129b9e4caee1dce8c0d
Niougat <65818605+Niougat@users.noreply.github.com>
2020-06-01 15:15:02 +0200

    Update repas.js

--- .gitignore/repas.js
+++ .gitignore/repas.js
@@ -1,7 +1,7 @@
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
 
-client.login("NzE0NDQ1MzgxNDM3NjIwMjk2.XtFGdg.zQarEu23vxJC_XsuKFWeWFWSPhA");
+client.login(process.env.TOKEN);
 
 var prefix = ("a!");
 
